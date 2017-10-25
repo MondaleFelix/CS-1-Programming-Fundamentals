e04934ef743c0a1a4c34561a95096b4c2e00f2f6
Mondale Felix <mondalefelix@yahoo.com>
2017-10-24 19:23:08 -0700

    intial commit

--- Herd_Immunity_Project/person.py
+++ Herd_Immunity_Project/person.py
@@ -2,6 +2,8 @@ import random
 # TODO: Import the virus clase
 
 class Person(object):
+
+
     '''
     Person objects will populate the simulation.
 
@@ -44,14 +46,15 @@ class Person(object):
     def __init__(self, _id, is_vaccinated, infected=None):
         # TODO:  Finish this method.  Follow the instructions in the class documentation
         # to set the corret values for the following attributes.
-        self._id = None
-        self.is_vaccinated = None
-        self.is_alive = None
-        self.infected = None
+        self._id = _id
+        self.is_vaccinated = is_vaccinated
+        self.is_alive = True
+        self.infected = infected
 
 
     def did_survive_infection():
+
         # TODO:  Finish this method. Follow the instructions in the class documentation
         # for resolve_infection.  If person dies, set is_alive to False and return False.
-        # If person lives, set is_vaccinated = True, infected = None, return True.  
+        # If person lives, set is_vaccinated = True, infected = None, return True.
         pass
